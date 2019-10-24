0c6a575f3aa9e20a504399baccb753f21418db3d
James Holton <james_holton@yahoo.co.uk>
2019-10-24 16:23:19 +0100

    Refactored and tidied up

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,34 +1,62 @@
+# frozen_string_literal: true
+# TicTacToe Class deals with all the logic for playing the game
 class TicTacToe
-  def initialize()
-    @matchGrid = [['','',''],['','',''],['','','']]
-    @lastPlayed = ''
+  def initialize
+    @match_grid = [['', '', ''], ['', '', ''], ['', '', '']]
+    @last_played = ''
   end
 
-  def play(noughtOrCross,column,row)
-    validateMove(noughtOrCross,column,row)
-    @matchGrid[column-1][row-1] = noughtOrCross
-    @lastPlayed = noughtOrCross
-    checkForWin() ? "Winner" : @matchGrid
-
+  def play(nought_or_cross, column, row)
+    validate_move(nought_or_cross, column, row)
+    make_move(nought_or_cross, column, row)
+    check_response(nought_or_cross)
   end
 
   private
-  
-  def validateMove(noughtOrCross,column,row)
-    raise "It's not your turn" if noughtOrCross == @lastPlayed
-    raise "Row/column has to be between 1 and 3" if !column.between?(1,3) || !row.between?(1,3)
-    raise "Move has to be an 'X' or a '0'" if !['X','0'].include? noughtOrCross
+
+  def validate_move(nought_or_cross, column, row)
+    raise "It's not your turn" if nought_or_cross == @last_played
+    raise 'Row/column has to be between 1 and 3' if !column.between?(1, 3) || !row.between?(1, 3)
+    raise 'This box is taken' if @match_grid[column - 1][row - 1] != ''
+    raise "Move has to be an 'X' or a '0'" unless %w[X 0].include? nought_or_cross
   end
 
-  def checkForWin()
-    winner = false;
-    m = @matchGrid
-    for x in 0..2
-      if (['X', '0'].include?(m[x][0]) && [m[x][0],m[x][1],m[x][2]].uniq.count == 1) || (['X','0'].include?(m[0][x]) && [m[0][x],m[1][x],m[2][x]].uniq.count == 1)
+  def make_move(nought_or_cross, column, row)
+    @match_grid[column - 1][row - 1] = nought_or_cross
+    @last_played = nought_or_cross
+  end
+
+  def check_response(nought_or_cross)
+    if check_for_win
+      "#{nought_or_cross} is the Winner"
+    elsif check_for_draw
+      "It's a draw"
+    else
+      @match_grid
+    end
+  end
+
+  def check_for_win
+    winner = false
+    m = @match_grid
+    (0..2).each do |x|
+      if (['X', '0'].include?(m[x][0]) && [m[x][0], m[x][1], m[x][2]].uniq.count == 1) ||
+         (['X', '0'].include?(m[0][x]) && [m[0][x], m[1][x], m[2][x]].uniq.count == 1)
         winner = true
       end
+    end
+    unless winner
+      winner = true if ['X', '0'].include?(m[0][0]) && [m[0][0], m[1][1], m[2][2]].uniq.count == 1
+      winner = true if ['X', '0'].include?(m[2][0]) && [m[2][0], m[1][1], m[0][2]].uniq.count == 1
     end
     winner
   end
 
+  def check_for_draw
+    spaces = 0
+    (0..2).each do |x|
+      spaces += @match_grid[x].count('')
+    end
+    spaces == 0
+  end
 end
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,8 +1,9 @@
-require "tic_tac_toe.rb"
+# frozen_string_literal: true
+
+require 'tic_tac_toe.rb'
 
 describe TicTacToe do
-  
-  context "Single move, full match array" do
+  context 'Single move, full match array' do
     it "Can place 'X' in position [1,1]" do
       expect(subject.play('X', 1, 1)).to eq [['X', '', ''], ['', '', ''], ['', '', '']]
     end
@@ -16,44 +17,80 @@ describe TicTacToe do
       expect(subject.play('X', 2, 1)).to eq [['', '', ''], ['X', '', ''], ['', '', '']]
     end
     it "Can place 'X' in position [3,1]" do
-      expect(subject.play('X', 3, 1)).to eq [['', '', ''], ['', '', ''],['X', '', '']]
+      expect(subject.play('X', 3, 1)).to eq [['', '', ''], ['', '', ''], ['X', '', '']]
     end
     it "Can place '0' in position [3,2]" do
       expect(subject.play('0', 3, 2)).to eq [['', '', ''], ['', '', ''], ['', '0', '']]
     end
   end
 
-  context "Single illegal move" do
-    it "tries to make a move outside the board" do
-      expect { subject.play('X', 4, 1) }.to raise_error("Row/column has to be between 1 and 3") 
+  context 'Single illegal move' do
+    it 'tries to make a move outside the board' do
+      expect { subject.play('X', 4, 1) }.to raise_error('Row/column has to be between 1 and 3')
     end
-    it "move has to be an X or a 0" do
-      expect { subject.play('Z', 3, 1) }.to raise_error("Move has to be an 'X' or a '0'") 
+    it 'move has to be an X or a 0' do
+      expect { subject.play('Z', 3, 1) }.to raise_error("Move has to be an 'X' or a '0'")
     end
   end
 
-  context "Multiple move tests" do
-    it "Plays an X, then a 0" do
+  context 'Multiple move tests' do
+    it 'Plays an X, then a 0' do
       subject.play('X', 1, 1)
-      expect(subject.play('0', 2, 2)).to eq [['X', '', ''], ['','0',''], ['', '', '']]
+      expect(subject.play('0', 2, 2)).to eq [['X', '', ''], ['', '0', ''], ['', '', '']]
     end
 
-    it "Plays an X, then a 0, then an X" do
+    it 'Plays an X, then a 0, then an X' do
       subject.play('X', 1, 1)
       subject.play('0', 2, 2)
       expect(subject.play('X', 3, 3)).to eq [['X', '', ''], ['', '0', ''], ['', '', 'X']]
     end
 
-    it "Checks X or 0 is not played twice in a row" do
+    it 'Checks X or 0 is not played twice in a row' do
       subject.play('X', 1, 1)
-      expect { subject.play('X', 2, 1) }.to raise_error("It's not your turn") 
+      expect { subject.play('X', 2, 1) }.to raise_error("It's not your turn")
+    end
+
+    it 'Checks box is not already takes' do
+      subject.play('X', 1, 1)
+      expect { subject.play('0', 1, 1) }.to raise_error('This box is taken')
     end
   end
 
-  context "Winning moves" do
+  context 'Winning moves' do
     it "checks for a win if top row is all X's" do
-      subject.instance_variable_set(:@matchGrid,[['X', 'X', ''] ,['', '', ''], ['', '', '']])
-      expect(subject.play('X', 1, 3)).to eq 'Winner'
+      subject.instance_variable_set(:@match_grid, [['X', 'X', ''], ['', '', ''], ['', '', '']])
+      expect(subject.play('X', 1, 3)).to eq 'X is the Winner'
+    end
+    it "checks for a win if middle row is all 0's" do
+      subject.instance_variable_set(:@match_grid, [['', '', ''], ['0', '', '0'], ['', '', '']])
+      expect(subject.play('0', 2, 2)).to eq '0 is the Winner'
+    end
+    it "checks for a win if bottom row is all 0's" do
+      subject.instance_variable_set(:@match_grid, [['', '', ''], ['', '', ''], ['', '0', '0']])
+      expect(subject.play('0', 3, 1)).to eq '0 is the Winner'
+    end
+    it "checks for a win if left column is all 0's" do
+      subject.instance_variable_set(:@match_grid, [['0', '', ''], ['0', '', ''], ['', '', '']])
+      expect(subject.play('0', 3, 1)).to eq '0 is the Winner'
+    end
+    it "checks for a win if middle column is all X's" do
+      subject.instance_variable_set(:@match_grid, [['', 'X', ''], ['', '', ''], ['', 'X', '']])
+      expect(subject.play('X', 2, 2)).to eq 'X is the Winner'
+    end
+    it "checks for a win if right column is all 0's" do
+      subject.instance_variable_set(:@match_grid, [['', '', '0'], ['', '', '0'], ['', '', '']])
+      expect(subject.play('0', 3, 3)).to eq '0 is the Winner'
+    end
+    it "checks for a win if diagonal is all 0's" do
+      subject.instance_variable_set(:@match_grid, [['0', '', ''], ['', '', ''], ['', '', '0']])
+      expect(subject.play('0', 2, 2)).to eq '0 is the Winner'
+    end
+  end
+
+  context 'Drawing move' do
+    it 'checks for a draw if all the places are filled' do
+      subject.instance_variable_set(:@match_grid, [%w[0 X O], %w[0 X 0], ['X', '0', '']])
+      expect(subject.play('X', 3, 3)).to eq "It's a draw"
     end
   end
 end
