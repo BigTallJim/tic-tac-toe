f04b1c4f1b658a4c56822f97a150a18aaff82331
James Holton <james_holton@yahoo.co.uk>
2019-10-24 14:10:25 +0100

    validates that move is 0 or X

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,8 +3,20 @@ class TicTacToe
     @matchGrid = [['','',''],['','',''],['','','']]
   end
 
-  def play(xOrY,column,row)
-    @matchGrid[column-1][row-1] = xOrY
+  def play(noughtOrCross,column,row)
+    validateMove(noughtOrCross,column,row)
+    @matchGrid[column-1][row-1] = noughtOrCross
     @matchGrid
+  end
+
+  private
+  
+  def validateMove(noughtOrCross,column,row)
+    if !column.between?(1,3) || !row.between?(1,3)
+      raise "Row/column has to be between 1 and 3"
+    end
+    if !['X','0'].include? noughtOrCross
+      raise "Move has to be an 'X' or a '0'"
+    end
   end
 end
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -18,6 +18,18 @@ describe TicTacToe do
     it "Can place 'X' in position [3,1]" do
       expect(subject.play('X',3,1)).to eq [['','',''],['','',''],['X','','']]
     end
+    it "Can place '0' in position [3,2]" do
+      expect(subject.play('0',3,2)).to eq [['','',''],['','',''],['','0','']]
+    end
+  end
+
+  context "Single illegal move" do
+    it "tries to make a move outside the board" do
+      expect { subject.play('X',4,1) }.to raise_error("Row/column has to be between 1 and 3") 
+    end
+    it "move has to be an X or a 0" do
+      expect { subject.play('Z',3,1) }.to raise_error("Move has to be an 'X' or a '0'") 
+    end
   end
 end
 
